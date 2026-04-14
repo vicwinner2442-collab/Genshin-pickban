6445d2889aa0eff3e27be51d9ffade2858806f7a
vicwinner2442-collab <vic.winner2442@gmail.com>
2026-04-14 21:33:04 +0300

    Update link preview description

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,20 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Genshin Pickban",
-  description: "Драфтилка по Genshin",
+  description: "Сайт для батлів по Геншину. Драфти, піки, бани, хрести -- все на місці",
+  openGraph: {
+    title: "Genshin Pickban",
+    description: "Сайт для батлів по Геншину. Драфти, піки, бани, хрести -- все на місці",
+  },
+  twitter: {
+    card: "summary",
+    title: "Genshin Pickban",
+    description: "Сайт для батлів по Геншину. Драфти, піки, бани, хрести -- все на місці",
+  },
   icons: {
-    icon: "/icon.jpg",
-    shortcut: "/icon.jpg",
-    apple: "/icon.jpg",
+    icon: "/icon.jpg?v=2",
+    shortcut: "/icon.jpg?v=2",
+    apple: "/icon.jpg?v=2",
   },
 };
 
